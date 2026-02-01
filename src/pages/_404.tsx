c2e311dd91ba0939e82f982ac4bf4843711df43e
Eepyamre <eepyamre@gmail.com>
2026-02-01 18:02:21 +0300

    mobile styles

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,16 +1,14 @@
 import { Layout } from '@/components';
 
+import css from './_404.module.scss';
+
 export const NotFound = () => {
   return (
     <Layout>
-      <h1
-        style={{
-          fontSize: '10rem',
-          textAlign: 'center',
-        }}
-      >
-        404
-      </h1>
+      <div class={css.wrapper}>
+        <h1 class={css.title}>404</h1>
+        <p class={css.text}>Page not found</p>
+      </div>
     </Layout>
   );
 };
