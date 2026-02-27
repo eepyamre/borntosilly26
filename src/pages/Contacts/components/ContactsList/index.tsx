41f388e5a22872bb220922d2f46e3a527d6af68c
Eepyamre <eepyamre@gmail.com>
2026-02-27 09:59:45 +0300

    update thread link

--- src/pages/Contacts/components/ContactsList/index.tsx
+++ src/pages/Contacts/components/ContactsList/index.tsx
@@ -19,7 +19,7 @@ export const ContactsList = () => {
         </Link>
         <span>
           on the{' '}
-          <Link href="https://boards.4chan.org/mlp/thread/42992735">
+          <Link href="https://boards.4chan.org/mlp/thread/43068627">
             {' '}
             4chan thread
           </Link>
